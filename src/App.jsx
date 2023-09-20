842f35d4c8c50ae32fd1494e38c8d7f673dd94df
Jasser Gharb <123377848+G-Jasser@users.noreply.github.com>
2023-09-20 08:50:20 +0100

    add job search section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { FindDreamSection } from './components/FindDreamSection'
 import { GeneralFooter } from './components/FooterGeneral'
 import { JobCategoriesSection } from './components/JobCategoriesSection'
+import { JobSearchSection } from './components/JobSearchSection'
 import LandingPage from './components/LandingPage'
 import { NavbarLandingPage } from './components/NavbarLandingPageUnsigned'
 
 function App() {
   return (
     <>
-      <NavbarLandingPage/>
-      <FindDreamSection/>
-      <JobCategoriesSection/>
-      <GeneralFooter/>
+      <NavbarLandingPage />
+      <FindDreamSection />
+      <JobCategoriesSection />
+      <JobSearchSection />
+      <GeneralFooter />
     </>
   )
 }
---
+++ src/components/JobSearchSection.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "/src/css/style.css";
+import { JobCard } from "./JobCard";
+import { JobSearchBar } from "./JobSearchBar";
+
+export const JobSearchSection = () => {
+    return (
+        <div className="section-container JobSearchSection">
+            <div className="max-width-2 main-container">
+                <div className="main-container-1">
+                    <div className="title-1">Search Posted Jobs</div>
+                    <JobSearchBar />
+                    <div className="list-scrollable">
+                        <JobCard />
+                        <JobCard />
+                        <JobCard />
+                        <JobCard />
+                        <JobCard />
+                        <JobCard />
+                    </div>
+                </div>
+                <div className="image-wrapper">
+                    <img src="/logos/logo-full.png" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
