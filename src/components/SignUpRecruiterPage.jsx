2afd741e50fa1af930a51f18cc96900ab37e4b5a
Jasser Gharb <123377848+G-Jasser@users.noreply.github.com>
2023-09-21 11:15:45 +0100

    add sign up pages

---
+++ src/components/SignUpRecruiterPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavbarLandingPage } from './NavbarLandingPageUnsigned'
+import { SignUpRecruiter } from './SignUpRecruiter'
+import { GeneralFooter } from './GeneralFooter'
+
+const SignUpRecruiterPage = () => {
+  return (
+    <>
+        <NavbarLandingPage/>
+        <SignUpRecruiter/>
+        <GeneralFooter/>
+    </>
+
+  )
+}
+
+export default SignUpRecruiterPage
---
+++ src/components/SignUpSeekerPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavbarLandingPage } from './NavbarLandingPageUnsigned'
+import { SignUpSeeker } from './SignUpSeeker'
+import { GeneralFooter } from './GeneralFooter'
+
+const SignUpSeekerPage = () => {
+  return (
+    <>
+        <NavbarLandingPage/>
+        <SignUpSeeker/>
+        <GeneralFooter/>
+    </>
+
+  )
+}
+
+export default SignUpSeekerPage
