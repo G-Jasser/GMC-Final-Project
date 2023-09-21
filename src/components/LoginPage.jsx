0d2231433dd5d080937a274f9f615365d86ac572
Jasser Gharb <123377848+G-Jasser@users.noreply.github.com>
2023-09-21 11:12:06 +0100

    add login page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,29 +1,16 @@
-import React from "react";
-import "/src/css/style.css";
+import React from 'react'
+import { NavbarLandingPage } from './NavbarLandingPageUnsigned'
+import { LoginSection } from './LoginSection'
+import { GeneralFooter } from './GeneralFooter'
 
-export const LoginPage = ({ className }) => {
+const LoginPage = () => {
   return (
-    <div className={`login form-page ${className}`}>
-      <div className="container">
-        <div className="title-sign">Log in</div>
-        <div className="input-frame">
-            <label className="text-wrapper">Email:</label>
-          <input className="textfield" type="email" />
-        </div>
-        <div className="input-frame">
-          <label className="text-wrapper">Password:</label>
-          <input className="textfield" type="password" />
-          <span className="info">Forgot password?</span>
-        </div>
-        <div className="double-button-container">
-          <button className="button-styled primary-button">
-            Sign up
-          </button>
-          <button className="button-styled secondary-button">
-            Connect
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-};
+    <>
+        <NavbarLandingPage/>
+        <LoginSection/>
+        <GeneralFooter/>
+    </>
+  )
+}
+
+export default LoginPage
---
+++ src/components/LoginSection.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "/src/css/style.css";
+
+export const LoginSection = ({ className }) => {
+  return (
+    <div className={`login form-page ${className}`}>
+      <div className="container">
+        <div className="title-sign">Log in</div>
+        <div className="input-frame">
+            <label className="text-wrapper">Email:</label>
+          <input className="textfield" type="email" />
+        </div>
+        <div className="input-frame">
+          <label className="text-wrapper">Password:</label>
+          <input className="textfield" type="password" />
+          <span className="info">Forgot password?</span>
+        </div>
+        <div className="double-button-container">
+          <button className="button-styled primary-button">
+            Sign up
+          </button>
+          <button className="button-styled secondary-button">
+            Connect
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
