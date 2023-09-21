e0f50e9b8ac1e3519a22803f94bb240da0b46d9a
Jasser Gharb <123377848+G-Jasser@users.noreply.github.com>
2023-09-21 13:17:17 +0100

    Create MockPostLists.js

---
+++ src/assets/MockPostLists.js
@@ -0,0 +1,112 @@
+export const MockJobList = [
+    {
+        job_id: 1,
+        job_title: "Software Engineer",
+        company_name: "Tech Company A",
+        location: "United States, San Francisco, 123 Main Street",
+        salary: "120,000 per year",
+        pending_applicants: 5,
+        job_requirement: "The candidate should have a strong background in computer science and software engineering, with proficiency in relevant programming languages and technologies. Experience in developing scalable and efficient software solutions is essential.",
+        job_responsibilities: "Responsibilities include designing, coding, testing, and debugging software applications. Collaborating with cross-functional teams, participating in code reviews, and continuously improving software development processes."
+    },
+    {
+        job_id: 2,
+        job_title: "Data Scientist",
+        company_name: "Data Analytics Inc.",
+        location: "Canada, Toronto, 456 Elm Street",
+        salary: "95,000 per year",
+        pending_applicants: 2,
+        job_requirement: "The ideal candidate should possess strong analytical and problem-solving skills. Proficiency in machine learning algorithms, statistical analysis, and data visualization is required. Experience with relevant programming languages and tools is a plus.",
+        job_responsibilities: "Responsibilities include analyzing large datasets, developing and implementing machine learning models, and interpreting results to drive data-driven decisions. Collaborating with data engineers and stakeholders to understand data needs and provide actionable insights."
+    },
+    {
+        job_id: 3,
+        job_title: "Product Manager",
+        company_name: "InnovateTech",
+        location: "United Kingdom, London, 789 Oak Avenue",
+        salary: "110,000 per year",
+        pending_applicants: 8,
+        job_requirement: "The candidate should have proven experience in product management, understanding market needs, and creating product roadmaps. Strong communication and leadership skills are essential to effectively collaborate with cross-functional teams.",
+        job_responsibilities: "Responsibilities include defining product vision and strategy, conducting market research, gathering and prioritizing product requirements, and working closely with development teams to ensure timely product delivery. Managing product launches and gathering feedback for continuous improvement."
+    },
+    {
+        job_id: 4,
+        job_title: "UX Designer",
+        company_name: "Design Innovations",
+        location: "Australia, Sydney, 567 Pine Road",
+        salary: "85,000 per year",
+        pending_applicants: 3,
+        job_requirement: "The ideal candidate should have a strong understanding of user-centered design principles and excellent visual design skills. Proficiency in design tools and a portfolio showcasing previous work are required.",
+        job_responsibilities: "Responsibilities include conducting user research, creating wireframes and prototypes, and collaborating with development teams to ensure a seamless user experience. Iteratively testing and refining designs based on user feedback and design best practices."
+    },
+    {
+        job_id: 5,
+        job_title: "Marketing Specialist",
+        company_name: "Digital Marketing Solutions",
+        location: "United States, New York, 101 Broadway",
+        salary: "70,000 per year",
+        pending_applicants: 1,
+        job_requirement: "The candidate should have a strong understanding of marketing strategies, digital platforms, and analytical skills. Experience in creating and executing marketing campaigns and measuring their success is essential.",
+        job_responsibilities: "Responsibilities include developing and implementing marketing campaigns across various channels, analyzing campaign performance, identifying trends, and making data-driven marketing decisions. Collaborating with the marketing team to optimize strategies and achieve marketing goals."
+    }
+]
+
+export const MockApplicantStatus = [
+    {
+        job_id: 1,
+        job_title: "Software Engineer",
+        company_name: "Tech Company A",
+        location: "United States, San Francisco, 123 Main Street",
+        salary: "120,000 per year",
+        application_status: "Declined"
+    },
+    {
+        job_id: 2,
+        job_title: "Data Scientist",
+        company_name: "Data Analytics Inc.",
+        location: "Canada, Toronto, 456 Elm Street",
+        salary: "95,000 per year",
+        application_status: "Pending"
+    },
+    {
+        job_id: 3,
+        job_title: "Product Manager",
+        company_name: "InnovateTech",
+        location: "United Kingdom, London, 789 Oak Avenue",
+        salary: "110,000 per year",
+        application_status: "Accepted"
+    }
+];
+
+export const MockApplicantList = [
+    {
+        applicant_id: 1,
+        applicant_fullname: "Joe Doe",
+        applicant_title: "Software Engineer",
+        application_date: "01/01/2020"
+    },
+    {
+        applicant_id: 2,
+        applicant_fullname: "Mary Smith",
+        applicant_title: "Data Scientist",
+        application_date: "02/15/2020"
+    },
+    {
+        applicant_id: 3,
+        applicant_fullname: "John Johnson",
+        applicant_title: "Product Manager",
+        application_date: "03/20/2020"
+    },
+    {
+        applicant_id: 4,
+        applicant_fullname: "Anna Lee",
+        applicant_title: "UX Designer",
+        application_date: "04/10/2020"
+    },
+    {
+        applicant_id: 5,
+        applicant_fullname: "Michael Brown",
+        applicant_title: "Marketing Specialist",
+        application_date: "05/05/2020"
+    }
+];
