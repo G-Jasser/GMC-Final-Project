c4f9e7db6570d207201c5a12a01d9a571f7d5a9d
Jasser Gharb <123377848+G-Jasser@users.noreply.github.com>
2023-09-22 11:38:41 +0100

    format document

--- client/src/JS/postSlice/postSlice.js
+++ client/src/JS/postSlice/postSlice.js
@@ -2,94 +2,94 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import axios from "axios"
 
 
-export const getPost=createAsyncThunk('post/get',async()=>{
-    try{
-let result=axios.get('http://localhost:5000/post/all')
-return result
-    }catch(error){
-    console.log(error)
+export const getPost = createAsyncThunk('post/get', async () => {
+    try {
+        let result = axios.get('http://localhost:5000/post/all')
+        return result
+    } catch (error) {
+        console.log(error)
     }
 })
 
-export const addPost=createAsyncThunk('post/add',async(user)=>{
-    try{
-let result=axios.post('http://localhost:5000/post/add',user)
-return result
-    }catch(error){
-    console.log(error)
+export const addPost = createAsyncThunk('post/add', async (user) => {
+    try {
+        let result = axios.post('http://localhost:5000/post/add', user)
+        return result
+    } catch (error) {
+        console.log(error)
     }
 })
-export const deletePost=createAsyncThunk('post/delete',async(id)=>{
-    try{
-let result=axios.delete(`http://localhost:5000/post/${id}`)
-return result
-    }catch(error){
-    console.log(error)
+export const deletePost = createAsyncThunk('post/delete', async (id) => {
+    try {
+        let result = axios.delete(`http://localhost:5000/post/${id}`)
+        return result
+    } catch (error) {
+        console.log(error)
     }
 })
 
-export const updatePost=createAsyncThunk('post/update',async({id, user})=>{
-    try{
-let result=axios.put(`http://localhost:5000/post/${id}`,user)
-return result
-    }catch(error){
-    console.log(error)
+export const updatePost = createAsyncThunk('post/update', async ({ id, user }) => {
+    try {
+        let result = axios.put(`http://localhost:5000/post/${id}`, user)
+        return result
+    } catch (error) {
+        console.log(error)
     }
 })
 
 
 const initialState = {
- post:null,
- status:null
+    post: null,
+    status: null
 }
 
 export const postSlice = createSlice({
-  name: 'post',
-  initialState,
-  reducers: {},
-  extraReducers:{
-    [getPost.pending]:(state)=>{
-        state.status="pending";
-    },
-    [getPost.fulfilled]:(state,action)=>{
-        state.status="success";
-        state.post=action.payload.data?.list
-    },
-    [getPost.rejected]:(state,action)=>{
-        state.status="failed";
-    },
-    [addPost.pending]:(state)=>{
-        state.status="pending";
-    },
-    [addPost.fulfilled]:(state,action)=>{
-        state.status="success";
-    },
-    [addPost.rejected]:(state,action)=>{
-        state.status="failed";
-    },
-    [deletePost.pending]:(state)=>{
-        state.status="pending";
-    },
-    [deletePost.fulfilled]:(state,action)=>{
-        state.status="success";
-    },
-    [deletePost.rejected]:(state,action)=>{
-        state.status="failed";
-    },
-    [updatePost.pending]:(state)=>{
-        state.status="pending";
-    },
-    [updatePost.fulfilled]:(state,action)=>{
-        state.status="success";
-        state.post=action.payload.data?.post
-    },
-    [updatePost.rejected]:(state,action)=>{
-        state.status="failed";
-    }
+    name: 'post',
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [getPost.pending]: (state) => {
+            state.status = "pending";
+        },
+        [getPost.fulfilled]: (state, action) => {
+            state.status = "success";
+            state.post = action.payload.data?.list
+        },
+        [getPost.rejected]: (state, action) => {
+            state.status = "failed";
+        },
+        [addPost.pending]: (state) => {
+            state.status = "pending";
+        },
+        [addPost.fulfilled]: (state, action) => {
+            state.status = "success";
+        },
+        [addPost.rejected]: (state, action) => {
+            state.status = "failed";
+        },
+        [deletePost.pending]: (state) => {
+            state.status = "pending";
+        },
+        [deletePost.fulfilled]: (state, action) => {
+            state.status = "success";
+        },
+        [deletePost.rejected]: (state, action) => {
+            state.status = "failed";
+        },
+        [updatePost.pending]: (state) => {
+            state.status = "pending";
+        },
+        [updatePost.fulfilled]: (state, action) => {
+            state.status = "success";
+            state.post = action.payload.data?.post
+        },
+        [updatePost.rejected]: (state, action) => {
+            state.status = "failed";
+        }
 
-  }
+    }
 })
 // Action creators are generated for each case reducer function
-export const {} = postSlice.actions
+export const { } = postSlice.actions
 
 export default postSlice.reducer
